4ab8033cb72a0d002cffbc002230ce42c7b74250
Documenter.jl <documenter@juliadocs.github.io>
2023-11-15 08:46:28 +0000

    build based on 1d425be

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = WGPUCanvas","category":"page"},{"location":"#WGPUCanvas","page":"Home","title":"WGPUCanvas","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for WGPUCanvas.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [WGPUCanvas]","category":"page"}]
+}
